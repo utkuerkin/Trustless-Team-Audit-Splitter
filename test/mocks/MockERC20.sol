a0ffdee42e91550c3a13a6aad00069f59849c4c6
utkuerkin <utkuerkin@gmail.com>
2025-01-13 16:52:33 +0300

    feat: factory changes and better

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -4,9 +4,21 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    uint8 private immutable _decimals;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_
+    ) ERC20(name, symbol) {
+        _decimals = decimals_;
+    }
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
     }
 } 
